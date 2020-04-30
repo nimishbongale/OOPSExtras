a164960122af537cfec537687c2e03cd8076800c
Nimish Bongale <nimishnb98@gmail.com>
2020-05-01 01:27:01 +0530

    Update Main.java

--- AssignmentOOADP/Main.java
+++ AssignmentOOADP/Main.java
@@ -1,1 +1,10 @@
+public class Main 
+{ 
+    public static void main(String args[]) 
+    { 
+        SOSCollection nc = new SOSCollection(); 
+        MessageApp ma = new MessageApp(nc); 
+        ma.printStatus(); 
+    } 
+}
 
