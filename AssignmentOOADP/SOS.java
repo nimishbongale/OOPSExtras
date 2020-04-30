d27bbcb3f424093b37dcff5ffb3277e39b7b4b77
Nimish Bongale <nimishnb98@gmail.com>
2020-05-01 01:24:48 +0530

    Create SOS.java

---
+++ AssignmentOOADP/SOS.java
@@ -0,0 +1,18 @@
+class SOS
+{  
+    String number; 
+    String message;
+    public SOS(String number) 
+    { 
+        this.number = number; 
+        this.message = "HELP";
+    } 
+    public String getNumber() 
+    { 
+        return number; 
+    }
+    public String getMessage() 
+    { 
+        return message; 
+    }
+}
